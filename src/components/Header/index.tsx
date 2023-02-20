bb38e1a830e0790092de53ba001c435f70668d60
GabrielS0ares <soaresgabriel33@gmail.com>
2023-02-20 09:08:35 -0300

    :sparkles: feat: incluindo botao no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import styles from "./styles.module.scss"
+import SingInButton from "../SingInButton"; 
 
 export function Header(){
     return(
@@ -10,6 +11,8 @@ export function Header(){
                     <a className={styles.active}>Home</a>
                     <a>Posts</a>
                 </nav>
+                
+                <SingInButton/>
             </div>
         </header>
     );
