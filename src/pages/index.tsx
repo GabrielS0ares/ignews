b7e79d80892f87010e087a5d74dca37cc4b0819c
GabrielS0ares <soaresgabriel33@gmail.com>
2023-02-24 12:28:52 -0300

    :sparkles: feat: alterando para setStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import SubscribeButton from '../components/SubscribeButton'
 
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 
 import styles from './home.module.scss'
 import { stripe } from '../services/stripe'
@@ -37,7 +37,7 @@ export default function Home({product} : HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => { 
+export const getStaticProps: GetStaticProps = async () => { 
 
   const price = await stripe.prices.retrieve('price_1MdYlVHKlsQF4oCiyyywyIWn', {
     expand: ['product']
@@ -56,6 +56,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
        product
-    }
+    },revalidate : 60 * 60 * 24,
   }
 }
