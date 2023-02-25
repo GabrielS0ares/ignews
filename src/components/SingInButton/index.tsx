f99d535c4705849acd28c3edd68ef5481360d2ea
GabrielS0ares <soaresgabriel33@gmail.com>
2023-02-25 14:37:14 -0300

    :sparkles: feat: usando dados da autenticacao para o botao

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,24 +1,26 @@
 import styles from './styles.module.scss'
-
 import { FaGithub } from 'react-icons/fa'
 import {FiX} from  'react-icons/fi'
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export function SingInButton(){
-    const isUserLoggedIn = true;
+    const {data} = useSession()
 
-    return isUserLoggedIn ? (
+    return data ? (
         <button 
             type="button"
             className={styles.singinbutton}
+            onClick={() => signOut()}
         >
             <FaGithub color="#04d361"/>
-            NomeGitHub
+                {data.user.name}
             <FiX color='#737380' className={styles.closeIcon}/>
         </button>
     ) : (
         <button 
         type="button"
         className={styles.singinbutton}
+        onClick={() => signIn('github')}
     >
             <FaGithub color="#eba417"/>
             Sing in With Github
