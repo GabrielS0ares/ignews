6a5227350346634ebddf6442e7c423c711bb831a
GabrielS0ares <soaresgabriel33@gmail.com>
2023-02-25 18:57:28 -0300

    :sparkles: feat: cadastro de dados no faubaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
-
+import { fauna } from '../../../services/fauna';
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -13,12 +15,23 @@ export const authOptions = {
     // ...add more providers here
   ],
   callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
+    async signIn({ user }) {
 
-      console.log(user)
-      return true
+      const {email} = user
+
+      try{
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            {data: {email}}
+          )
+        )
+        return true
+      }
+      catch{
+        return false
+      }
     },
   }
-
 }
 export default NextAuth(authOptions)
