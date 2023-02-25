b234e2d52b39788951aa675991e69a8a7be2457b
GabrielS0ares <soaresgabriel33@gmail.com>
2023-02-25 18:36:21 -0300

    :construction: feat: implementando callback do next auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -9,5 +12,13 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+
+      console.log(user)
+      return true
+    },
+  }
+
 }
 export default NextAuth(authOptions)
