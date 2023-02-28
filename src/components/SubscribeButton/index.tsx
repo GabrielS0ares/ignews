087155154f4ee622cb17b0942d35919560f7e6d2
GabrielS0ares <soaresgabriel33@gmail.com>
2023-02-27 22:51:47 -0300

    :sparkles: feat: chamando o end point para subscribe no stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { useSession, signIn } from 'next-auth/react';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,10 +6,22 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({priceId} : SubscribeButtonProps){
+    const {data} = useSession();
+
+
+    
+    function handleSubscribe(){
+        if(!data){
+            signIn('github')
+            return
+        }
+    }
+
     return (
         <button
             type='button'
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
         Subscribe Now
         </button>
