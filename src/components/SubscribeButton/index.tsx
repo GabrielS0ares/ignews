2ecfc34ca0eea0918859c757067208dfb1cdd0b8
GabrielS0ares <soaresgabriel33@gmail.com>
2023-03-08 23:02:55 -0300

    :sparkles: feat: configurando o botao de subscribe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { useSession, signIn } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -10,10 +12,23 @@ export function SubscribeButton({priceId} : SubscribeButtonProps){
 
 
     
-    function handleSubscribe(){
+    async function handleSubscribe(){
         if(!data){
             signIn('github')
             return
+        }
+
+        try {
+            const response = await api.post('/subscribe')
+
+            const {sessionId} = response.data
+
+            const stripe = await getStripeJs();
+
+            await stripe.redirectToCheckout({sessionId : sessionId.id})
+        }
+        catch(err){
+           alert(err.message)
         }
     }
 
